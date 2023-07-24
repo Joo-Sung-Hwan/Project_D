5051cd0b96706cf5d8ea4bf057d2343268033ab8
Park_Sang_Wook <124150788+parksangwook0207@users.noreply.github.com>
2023-07-24 17:39:29 +0900

    7.24 서버UI 겹쳤을 때 경고UI띄움

--- Assets/상욱/Script/StartSettingManager.cs
+++ Assets/상욱/Script/StartSettingManager.cs
@@ -7,6 +7,7 @@ using TMPro;
 public class StartSettingManager : MonoBehaviour
 {
     [SerializeField] private TMP_InputField playerNametxt;
+    [SerializeField] private TMP_InputField c_playerNametext;
     [SerializeField] private Image imageUI;
     // imageUI 이미지안에 있는 텍스트
     [SerializeField] private TMP_Text playerLasttxt; // playerNametxt에 쓴 닉네임을 입력
@@ -16,16 +17,25 @@ public class StartSettingManager : MonoBehaviour
     [HideInInspector] public string playersetName;
     // 방생성 UI
     [SerializeField] private Image roomCreate;
+    // 닉네임 비교UI
+    [SerializeField] private Image nickNameCheck;
+
+    bool isCreatecheck = false;
+
 
     string playerLast;
+
+    string roomTexttext;
 
     void Start()
     {
         imageUI.transform.gameObject.SetActive(false);
         roomCreate.transform.gameObject.SetActive(false);
+        nickNameCheck.transform.gameObject.SetActive(false);
     }
 
 
+    // 마지막 질문창 텍스트
     public void Imagetext()
     {
         playerLast = playersetName;
@@ -36,19 +46,30 @@ public class StartSettingManager : MonoBehaviour
     // 마지막 닉네임imageUI 나오기전 확인버튼
     public void OnButton()
     {
-        playersetName = playerNametxt.text;
+        if(isCreatecheck == false)
+        {
+            playersetName = playerNametxt.text;
+        }
+        else
+        {
+            playersetName = c_playerNametext.text;
+        }
         imageUI.transform.gameObject.SetActive(true);
         Imagetext();
     }
 
+    // 닉네임 마지막질문창에서 "아니요"누르면 마지막질문창이 꺼짐
     public void OnOffNickNameUI()
     {
+        isCreatecheck = false;
+        c_playerNametext.text = string.Empty;
         imageUI.transform.gameObject.SetActive(false);
     }
 
     // 방생성버튼
     public void OnRoomCreate()
     {
+        isCreatecheck = true;
         roomCreate.transform.gameObject.SetActive(true);
     }
 
@@ -56,5 +77,10 @@ public class StartSettingManager : MonoBehaviour
     public void OnRoomCreateOff()
     {
         roomCreate.transform.gameObject.SetActive(false);
+    }
+
+    public void SetRoomText()
+    {
+       
     }
 }
