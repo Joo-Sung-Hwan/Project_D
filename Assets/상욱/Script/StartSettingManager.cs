a141d31e5e728c70af969ecef8e6fad7561d0aa8
Park_Sang_Wook <124150788+parksangwook0207@users.noreply.github.com>
2023-07-25 16:45:45 +0900

    7.25서버UI랑 닉네임UI분리작업완료

--- Assets/상욱/Script/StartSettingManager.cs
+++ Assets/상욱/Script/StartSettingManager.cs
@@ -7,68 +7,20 @@ using Photon.Pun;
 using Photon.Realtime;
 
 public class StartSettingManager : MonoBehaviour
-{
-    [SerializeField] private TMP_InputField playerNametxt;
-    [SerializeField] private TMP_InputField c_playerNametext;
-    [SerializeField] private Image imageUI;
-    // imageUI 이미지안에 있는 텍스트
-    [SerializeField] private TMP_Text playerLasttxt; // playerNametxt에 쓴 닉네임을 입력
-    // 마지막 질문 텍스트
-    [SerializeField] private TMP_Text Questionstxt;
-
-    [HideInInspector] public string playersetName;
+{ 
     // 방생성 UI
     [SerializeField] private Image roomCreate;
-    // 닉네임 비교UI
-    [SerializeField] private Image nickNameCheck;
-
+ 
     [SerializeField] private GameObject prefab;
     [SerializeField] private Transform parent;
 
-    bool isCreatecheck = false;
-
-
-    string playerLast;
-
     string roomTexttext;
+    bool isCreatecheck = false;
 
     void Start()
     {
-        imageUI.transform.gameObject.SetActive(false);
         roomCreate.transform.gameObject.SetActive(false);
-        nickNameCheck.transform.gameObject.SetActive(false);
-    }
-
-
-    // 마지막 질문창 텍스트
-    public void Imagetext()
-    {
-        playerLast = playersetName;
-        playerLasttxt.text = $"캐릭터 이름 : {playerLast}";
-        Questionstxt.text = $"캐릭터 이름은 {playerLast} 입니다. 이대로 진행하시겠습니까?";
-    }
-
-    // 마지막 닉네임imageUI 나오기전 확인버튼
-    public void OnButton()
-    {
-        if(isCreatecheck == false)
-        {
-            playersetName = playerNametxt.text;
-        }
-        else
-        {
-            playersetName = c_playerNametext.text;
-        }
-        imageUI.transform.gameObject.SetActive(true);
-        Imagetext();
-    }
-
-    // 닉네임 마지막질문창에서 "아니요"누르면 마지막질문창이 꺼짐
-    public void OnOffNickNameUI()
-    {
-        isCreatecheck = false;
-        c_playerNametext.text = string.Empty;
-        imageUI.transform.gameObject.SetActive(false);
+        //nickNameCheck.transform.gameObject.SetActive(false);
     }
 
     // 방생성버튼
