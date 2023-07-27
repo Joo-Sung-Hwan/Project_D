9e24c781267d29c104cbfe33714e01a363316a80
Park_Sang_Wook <124150788+parksangwook0207@users.noreply.github.com>
2023-07-27 19:30:34 +0900

    7.27인게임UI 프렌드UI합침

--- Assets/상욱/Script/InGameUI.cs
+++ Assets/상욱/Script/InGameUI.cs
@@ -7,62 +7,71 @@ public class InGameUI : MonoBehaviour
 {
     [SerializeField] private Image[] prefabCharacter;
     [SerializeField] private Transform[] parent;
-    
+
     // 게임오브젝트를 담는 곳(부모)
     [SerializeField] private Transform callCharacter1;
     [SerializeField] private Transform callCharacter2;
     [SerializeField] private Transform callCharacter3;
     // 게임오브젝트생성(자식)
     [SerializeField] private GameObject create1;
-    //[SerializeField] private GameObject create2;
-    //[SerializeField] private GameObject create3;
+
+
+    [SerializeField] private Image firendList;
 
     public int randomInt;
 
-    RaycastHit raycasthit;
+    bool ischeck = true;
+
     // Start is called before the first frame update
     void Start()
+    {
+        SetBottomImage();
+        firendList.transform.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // 레벌업
+    public void OnLevelUP()
+    {
+        Debug.Log("레벨업");
+    }
+
+    public void SetBottomImage()
     {
         for (int i = 0; i < parent.Length; i++)
         {
             randomInt = Random.Range(0, 9);
             Instantiate(prefabCharacter[randomInt], parent[i]);
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    // 레벌업
-    public void OnLevelUP() 
-    {
-        Debug.Log("레벨업");
     }
 
     // 새로고침
     public void OnRefresh()
     {
         Debug.Log("새로고침");
-        for(int i = 0; i < parent.Length; i++)
-        {
-            //Destroy(parent[i].transform.GetChild(0).gameObject);
-            //Image[] image = prefabCharacter;
-            
-        }
         for (int i = 0; i < parent.Length; i++)
         {
             randomInt = Random.Range(0, 9);
-            Instantiate(prefabCharacter[randomInt], parent[i]);
+            parent[i].transform.GetChild(0).GetComponent<Image>().sprite = prefabCharacter[randomInt].sprite;
         }
     }
     // 캐릭터 소환
     public void OnCharacterImage()
     {
-        Debug.Log("캐릭터 소환");        
+        Debug.Log("캐릭터 소환");
     }
+
+    public void OnFirendList()
+    {
+        firendList.transform.gameObject.SetActive(ischeck);
+        ischeck = !ischeck;
+    }
+
 
 
 }
