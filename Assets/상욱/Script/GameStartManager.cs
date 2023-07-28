4056aca9d8ac613aa31bb4b2a2901b41e805a048
Park_Sang_Wook <124150788+parksangwook0207@users.noreply.github.com>
2023-07-28 19:46:48 +0900

    7.28 게임시작하기전UI 구현 60퍼정도 완료
    필요 있는 거 순서바꾸고 필요없는건 숫자를 지워서 밑으로 내림

--- Assets/상욱/Script/GameStartManager.cs
+++ Assets/상욱/Script/GameStartManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class GameStartManager : MonoBehaviour
 {
@@ -16,33 +17,55 @@ public class GameStartManager : MonoBehaviour
     [SerializeField] private Image ready_Image3_3;
     [SerializeField] private Image ready_Image4_4;
 
+    [SerializeField] private TMP_Text nameText;
+
     //[SerializeField] private Color ready_Image_color;
     //[SerializeField] private Color not_ReadyImage_color;
 
     bool ischack = false;
 
+    private void Start()
+    {
+        ready_Image1_1.gameObject.SetActive(false);
+        ready_Image2_2.gameObject.SetActive(false);
+        ready_Image3_3.gameObject.SetActive(false);
+        ready_Image4_4.gameObject.SetActive(false);
+    }
     // 버튼 
     public void OnReady_Image1()
     {
         Debug.Log("첫번째 버튼이 눌렸습니다.");
         ready_Image1.color = new Color(255, 244, 0, 255);
-        ready_Image1.gameObject.SetActive(ischack);
+        ready_Image1.gameObject.SetActive(!ischack);
         ischack = !ischack;
+        ready_Image1.gameObject.SetActive(ischack);
 
     }
     public void OnReady_Image2()
     {
         Debug.Log("두번째 버튼이 눌렸습니다.");
-        
+        ready_Image2.color = new Color(255, 244, 0, 255);
+        ready_Image2.gameObject.SetActive(!ischack);
+        ischack = !ischack;
+        ready_Image2.gameObject.SetActive(ischack);
+
     }
     public void OnReady_Image3()
     {
         Debug.Log("세번째 버튼이 눌렸습니다.");
-        
+        ready_Image3.color = new Color(255, 244, 0, 255);
+        ready_Image3.gameObject.SetActive(!ischack);
+        ischack = !ischack;
+        ready_Image3.gameObject.SetActive(ischack);
+
     }
     public void OnReady_Image4()
     {
         Debug.Log("네번째 버튼이 눌렸습니다.");
-       
+        ready_Image4.color = new Color(255, 244, 0, 255);
+        ready_Image4.gameObject.SetActive(!ischack);
+        ischack = !ischack;
+        ready_Image4.gameObject.SetActive(ischack);
+
     }
 }
