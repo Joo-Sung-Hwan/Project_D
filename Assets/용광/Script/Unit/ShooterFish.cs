d66993ea797a98098bc739c4d21dbbca01db68a0
dhf0527 <rbals0527@naver.com>
2023-08-29 19:56:22 +0900

    유닛 추가중, shooterFish 스킬 구현 필요

--- Assets/용광/Script/Unit/ShooterFish.cs
+++ Assets/용광/Script/Unit/ShooterFish.cs
@@ -2,20 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Parrot : Unit
+public class ShooterFish : Unit
 {
     public override void Init()
     {
         base.Init();
-        ud.element_type = Element_Type.fire;
+        ud.element_type = Element_Type.water;
         ud.atk_type = Attack_Type.normal;
         ud.attack = 30f;
-        ud.atkDelay = 0.5f;
-        ud.mana_type = Mana_Type.auto;
-        ud.maxMana = 2f;
+        ud.atkDelay = 1f;
+        ud.mana_type = Mana_Type.attack;
+        ud.maxMana = 5f;
         ud.curMana = 0f;
         ud.isBuy = false;
     }
+
     void Start()
     {
         Init();
@@ -23,7 +24,7 @@ public class Parrot : Unit
 
     protected override void Attack()
     {
-        //StartCoroutine(C_Attack(ud.atk_type, Damage_Type.physic, Debuff_Type.slow, 2f));
+        StartCoroutine(C_Attack(ud.atk_type, Damage_Type.physic));
     }
 
     //스킬 애니메이션 시작할 때 호출
@@ -39,10 +40,7 @@ public class Parrot : Unit
 
     public override IEnumerator Skill()
     {
-        particle.gameObject.SetActive(true);
-        particle.EffStart(1, 1, null);
-        SetMana(0);
-        canSkill = true;
+        anim.SetTrigger("skill");
         yield break;
     }
 }
